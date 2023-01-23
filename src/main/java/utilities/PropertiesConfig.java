a32ccd4cf22d11873538337641e00e094ecfe9ee
Curry Pham <64634056+CurryPham@users.noreply.github.com>
2023-01-23 11:36:06 +0700

    (Framework - Parallel Testing/ ThreadLocal/ Singleton Pattern)

--- src/main/java/utilities/PropertiesConfig.java
+++ src/main/java/utilities/PropertiesConfig.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.Properties;
 
 public class PropertiesConfig {
     private final Properties properties;
