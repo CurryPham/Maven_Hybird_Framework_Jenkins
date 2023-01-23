b019eb74897838d81825a586eba8b2bd002c3aca
Curry Pham <64634056+CurryPham@users.noreply.github.com>
2023-01-23 16:28:27 +0700

    Cucumber - Setup tool and project

--- maven-cucumber-bdd/src/test/java/stepDefinitions/facebookSteps.java
+++ maven-cucumber-bdd/src/test/java/stepDefinitions/facebookSteps.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
